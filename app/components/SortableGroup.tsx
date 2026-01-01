f975b23d0070db4872d88c8ce9d5e4a473f05937
sibtanu <sibtanu@gmail.com>
2026-01-01 09:30:44 -0500

    Sortable group alingment mobile

--- app/components/SortableGroup.tsx
+++ app/components/SortableGroup.tsx
@@ -71,7 +71,7 @@ export default function SortableGroup({
           <div
             {...attributes}
             {...listeners}
-            className="mt-6 cursor-grab active:cursor-grabbing opacity-0 group-hover:opacity-100 transition-opacity"
+            className="hidden sm:block mt-6 cursor-grab active:cursor-grabbing opacity-0 group-hover:opacity-100 transition-opacity"
           >
             <svg
               className="w-5 h-5 text-gray-400"
